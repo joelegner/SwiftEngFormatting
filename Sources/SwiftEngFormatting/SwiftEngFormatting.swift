798a490872f3eacbd572170beb9b2253d5ef44a4
Joe Legner <joelegner@gmail.com>
2024-08-24 10:28:26 -0400

    Added formatting code and tests

--- Sources/SwiftEngFormatting/SwiftEngFormatting.swift
+++ Sources/SwiftEngFormatting/SwiftEngFormatting.swift
@@ -1,2 +1,102 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+/* This is a prototype. Leave it here and create a new package some day.
+ Maybe even publish the package. It's kind of neat. */
+
+func significantDigitFormat(_ value: Double, digits: Int=3) -> String? {
+    // Format the value to three significant digits
+    let formatter = NumberFormatter()
+    
+    formatter.usesSignificantDigits = true
+    formatter.maximumSignificantDigits = digits
+    formatter.minimumSignificantDigits = digits
+    if let formattedValue = formatter.string(from: NSNumber(value: value)) {
+        return formattedValue
+    } else {
+        return nil
+    }
+}
+
+extension String.StringInterpolation {
+    mutating func appendInterpolation(_ value: Date) {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .full
+        
+        let dateString = formatter.string(from: value)
+        appendLiteral(dateString)
+    }
+    
+    mutating func appendInterpolation(sigdig value: Double) {
+        appendInterpolation(sigdig3: value)
+    }
+    
+    mutating func appendInterpolation(sigdig2 value: Double) {
+        if let formattedValue = significantDigitFormat(value, digits: 2) {
+            appendLiteral(formattedValue)
+        }
+    }
+    
+    mutating func appendInterpolation(sigdig3 value: Double) {
+        if let formattedValue = significantDigitFormat(value, digits: 3) {
+            appendLiteral(formattedValue)
+        }
+    }
+    
+    
+    mutating func appendInterpolation(sigdig4 value: Double) {
+        if let formattedValue = significantDigitFormat(value, digits: 4) {
+            appendLiteral(formattedValue)
+        }
+    }
+    
+    mutating func appendInterpolation(kips value: Double) {
+        if let formattedValue = significantDigitFormat(value, digits: 3) {
+            appendLiteral(formattedValue)
+        }
+    }
+    
+    mutating func appendInterpolation(ftin value: Double) {
+        let feet = Int(value)
+        let inchesPerFoot = 12.0
+        let fractionalFeetRemaining = abs(value - Double(feet))
+        let decimalInches = fractionalFeetRemaining * inchesPerFoot
+        let wholeInches = Int(decimalInches)
+        let denominator = 16
+        
+        func simplify(_ numerator: Int, _ denominator: Int) -> (Int, Int) {
+            var x = numerator
+            var y = denominator
+            while y != 0 {
+                let temp = y
+                y = x % y
+                x = temp
+            }
+            return (numerator / x, denominator / x)
+        }
+        
+        func formatResult(feet: Int, inches: Int, fraction: String = "") {
+            if abs(feet) > 0 {
+                appendLiteral("\(feet)'-\(inches)\(fraction)\"")
+            } else {
+                if value < 0 {
+                    appendLiteral("-\(inches)\(fraction)\"")
+                } else {
+                    appendLiteral("\(inches)\(fraction)\"")
+                }
+            }
+        }
+        
+        if decimalInches > Double(wholeInches) {
+            let numerator = Int(round((decimalInches - Double(wholeInches)) * Double(denominator)))
+            let (simpleNumerator, simpleDenominator) = simplify(numerator, denominator)
+            
+            if simpleNumerator == simpleDenominator {
+                formatResult(feet: feet, inches: wholeInches + 1)
+            } else {
+                formatResult(feet: feet, inches: wholeInches, fraction: " \(simpleNumerator)/\(simpleDenominator)")
+            }
+        } else {
+            formatResult(feet: feet, inches: wholeInches)
+        }
+    }
+}
--- Tests/SwiftEngFormattingTests/SwiftEngFormattingTests.swift
+++ Tests/SwiftEngFormattingTests/SwiftEngFormattingTests.swift
@@ -1,12 +1,31 @@
 import XCTest
 @testable import SwiftEngFormatting
 
-final class SwiftEngFormattingTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+// XCTest Documentation
+// https://developer.apple.com/documentation/xctest
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+// Defining Test Cases and Test Methods
+// https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+
+final class SwiftEngFormattingTests: XCTestCase {
+    func testFeetInchesFormatting() {
+        XCTAssertEqual("\(ftin:5.833333333)", "5'-10\"")
+        XCTAssertEqual("\(ftin:5.875)", "5'-10 1/2\"")
+        XCTAssertEqual("\(ftin:5.90625)", "5'-10 7/8\"")
+        XCTAssertEqual("\(ftin:5.90624)", "5'-10 7/8\"")
+        XCTAssertEqual("\(ftin:-5.90624)", "-5'-10 7/8\"")
+        XCTAssertEqual("\(ftin:0.515625)", "6 3/16\"")
+        XCTAssertEqual("\(ftin:-0.515625)", "-6 3/16\"")
+        XCTAssertEqual("\(ftin:0.75)", "9\"")
+        XCTAssertEqual("\(ftin:0.0)", "0\"")
+        XCTAssertEqual("\(ftin:-0.0)", "0\"")
+        XCTAssertEqual("\(ftin:-0.75)", "-9\"")
+    }
+    
+    func testSignificantDigitsFormatting() {
+        XCTAssertEqual("\(sigdig:0.00543243)", "0.00543")
+        XCTAssertEqual("\(sigdig2:0.00543243)", "0.0054")
+        XCTAssertEqual("\(sigdig3:0.00543243)", "0.00543")
+        XCTAssertEqual("\(sigdig4:0.00543243)", "0.005432")
     }
 }
